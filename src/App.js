0a471f6e8049ea1f16ee47196d915c071c71c570
Wendy de Kock <wendydekock@MacBook-Pro-van-Wendy.local>
2021-07-19 22:38:34 +0200

    feature - Added city search/autocomplete field using mapbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { fetchPlace } from './fetchPlace';
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState("");
+  const [autocompleteCities, setAutocompleteCities] = useState([]);
+  const [autocompleteErr, setAutocompleteErr] = useState("");
+
+  const handleCityChange = async (e) => {
+    setCity(e.target.value);
+    if (!city) return;
+
+    const res = await fetchPlace(city);
+    !autocompleteCities.includes(e.target.value) &&
+      res.features &&
+      setAutocompleteCities(res.features.map((place) => place.place_name));
+    res.error ? setAutocompleteErr(res.error) : setAutocompleteErr("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="placesAutocomplete">
+      <div className="placesAutocomplete__inputWrap">
+        <label htmlFor="city" className="label">
+          Your city
+          <span
+            className={autocompleteErr ? "inputError" : "inputErrorSkeleton"}
+          >
+            {autocompleteErr}
+          </span>
+        </label>
+        <input
+          list="places"
+          type="text"
+          id="city"
+          name="city"
+          onChange={handleCityChange}
+          value={city}
+          required
+          pattern={autocompleteCities.join("|")}
+          autoComplete="off"
+        />
+        <datalist id="places">
+          {autocompleteCities.map((city, i) => (
+            <option key={i}>{city}</option>
+          ))}
+        </datalist>
+        <span className="placesAutocomplete__hint">
+          *start typing and choose your city from the given options
+        </span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
